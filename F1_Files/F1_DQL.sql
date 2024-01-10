ddc6e93e5068cbd8ab4cb36ff335c5e47f6b2415
Lucas Pinheiro <115811060+lucaspinheiro27@users.noreply.github.com>
2024-01-10 18:02:33 -0300

    Add files via upload

--- F1_Files/F1_DQL.sql
+++ F1_Files/F1_DQL.sql
@@ -35,3 +35,31 @@ select count(*), sum(nr_carro), avg(nr_carro) from t_f1_resultado;
 --------------------------------------------------------------------------------
 select avg(nvl(nr_carro, 0)) from t_f1_resultado;
 --------------------------------------------------------------------------------
+select count(cd_equipe), count(distinct cd_equipe), count(all cd_equipe)
+from t_f1_piloto
+--------------------------------------------------------------------------------
+select count(all cd_resultado) from t_f1_resultado;
+--------------------------------------------------------------------------------
+select stddev_pop(all cd_resultado)
+from t_f1_resultado;
+--------------------------------------------------------------------------------
+select cd_equipe, count(cd_depto)
+from t_f1_equipe
+group by cd_equipe;
+--------------------------------------------------------------------------------
+select 'equipe', cd_equipe, count(nr_carro)
+from t_f1_piloto
+group by 'equipe', cd_equipe;
+--------------------------------------------------------------------------------
+select rownum, V.*
+from (select cd_equipe, count(nr_carro)
+from t_f1_piloto group by cd_equipe) V;
+--------------------------------------------------------------------------------
+select count(nr_carro)
+from t_f1_piloto
+group by cd_equipe;
+--------------------------------------------------------------------------------
+select cd_equipe, nr_titulos, count(nr_carro)
+from t_f1_piloto
+group by cd_equipe, nr_titulos;
+--------------------------------------------------------------------------------
